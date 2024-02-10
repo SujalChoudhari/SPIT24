8254af3006d57c82fac653bf27a1f96a6732934c
Soham <realsoham774@gmail.com>
2024-02-10 14:21:13 +0530

    i

--- src/app/(dashboard)/gantt/page.tsx
+++ src/app/(dashboard)/gantt/page.tsx
@@ -1,3 +1,9 @@
-export default function() {
-    return <></>
+export default function Gantt() {
+    return (
+        <div>
+            <h1 className=' ml-14 font-mono text-lg font-extrabold'>Tasks</h1>
+
+        </div>
+
+    )
 }
