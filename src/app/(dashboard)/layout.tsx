88c79f260bd31ecf9bb2eee866b00d5e92eb573b
Soham <realsoham774@gmail.com>
2024-02-10 21:53:55 +0530

    ui

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,8 +1,8 @@
 "use client";
-import React, { ReactNode, useEffect, useState } from "react";
+import React, { ReactNode, useEffect } from "react";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
-import { GanttChart, ChevronsLeftIcon, HomeIcon, ShoppingBagIcon, UsersIcon, BarChart2Icon, SettingsIcon, GanttChartSquare, Kanban, KanbanSquare, MailIcon, MailMinus, MailPlus, LucideAreaChart, Settings, Code2Icon, Tv2Icon, GlassWater, WindIcon, LucideVegan, ListVideo, KeyboardIcon, LogOut, ShieldQuestion, SidebarIcon } from "lucide-react";
+import { GanttChart, ChevronsLeftIcon, HomeIcon, ShoppingBagIcon, UsersIcon, BarChart2Icon, SettingsIcon, GanttChartSquare, Kanban, KanbanSquare, MailIcon, MailMinus, MailPlus, LucideAreaChart, Settings, Code2Icon, Tv2Icon, GlassWater, WindIcon, LucideVegan, ListVideo, KeyboardIcon, LogOut } from "lucide-react";
 import { usePathname } from "next/navigation";
 import { Avatar, AvatarImage } from "@/components/ui/avatar";
 import { useAuth } from "@/context/AuthContext";
@@ -31,63 +31,37 @@ interface SidebarProps {
     links: SidebarLinkProps[];
 }
 
-const Sidebar: React.FC<SidebarProps> = ({ links }) => {
-    const [isMobile, setIsMobile] = useState(false);
-
-    useEffect(() => {
-        const handleResize = () => {
-            setIsMobile(window.innerWidth <= 768); // Adjust the breakpoint as needed
-        };
-
-        window.addEventListener('resize', handleResize);
-        handleResize(); // Initial check on mount
-        return () => {
-            window.removeEventListener('resize', handleResize);
-        };
-    },);
-
-    return (
-        <div className={`flex flex-col h-full border-r bg-gray-100/40 ${isMobile ? 'md:hidden' : 'md:grid md:grid-rows-[auto_1fr]'}`}>
-            <div className={`flex items-center justify-between p-4 bg-gray-100/50 md:flex-col md:items-start md:gap-4 md:p-6 ${isMobile ? 'pb-16' : ''}`}>
-                <Link href="#">
-                    <p className="flex items-center gap-2 text-xl font-bold">
-                        <WindIcon className="w-6 h-6" />
-                        <span>FlowByte</span>
-                    </p>
-                </Link>
-            </div>
-            {isMobile && (
-                <div className="fixed bottom-0 left-0 right-0 bg-gray-100 p-4 flex flex-wrap justify-center">
-                    {links.map((link, index) => (
-                        <div key={index} className="m-2">
-                            {link.icon}
-                        </div>
-                    ))}
-                </div>
-            )}
-
-            <nav className={`flex-1 overflow-auto p-4 space-y-4 md:p-2 md:space-y-2 lg:space-y-4 ${isMobile ? 'hidden md:block' : ''}`}>
-                {links.map((link, index) => (
-                    <SidebarLink key={index} {...link} />
-                ))}
-            </nav>
+const Sidebar: React.FC<SidebarProps> = ({ links }) => (
+    <div className="flex flex-col h-full border-r bg-gray-100/40 md:grid md:grid-rows-[auto_1fr] md:bg-gray-100/50 lg:bg-gray-100/40">
+        <div className="flex items-center justify-between p-4 bg-gray-100/50 md:flex-col md:items-start md:gap-4 md:p-6">
+            <Link href="#">
+                <p className="flex items-center gap-2 text-xl font-bold">
+                    <WindIcon className="w-6 h-6" />
+                    <span>FlowByte</span>
+                </p>
+            </Link>
         </div>
-    );
-};
+        <nav className="flex-1 overflow-auto p-4 space-y-4 md:p-2 md:space-y-2 lg:space-y-4">
+            {links.map((link, index) => (
+                <SidebarLink key={index} {...link} />
+            ))}
+        </nav>
+    </div>
+);
+
 interface LayoutProps {
     children: ReactNode;
 }
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
     const auth = useAuth()
-    return <div className="grid min-h-screen w-full bg-gray-100/50 items-start md:grid-cols-[250px_1fr] lg:items-center xl:items-center">
+    return <div className="grid  bg-purple-100/50 w-full  items-start md:grid-cols-[250px_1fr] lg:items-center xl:items-center">
         <Sidebar
             links={[
                 { icon: <HomeIcon />, title: "Home", href: "/home" },
                 { icon: <KanbanSquare />, title: "Tasks", href: "/tasks" },
                 { icon: <GanttChartSquare />, title: "Timeline", href: "/gantt" },
                 { icon: <MailPlus />, title: "Invite", href: "/invite" },
-                { icon: <ShieldQuestion />, title: "Ask AI", href: "/ask-ai" },
                 { icon: <LucideAreaChart />, title: "Analytics", href: "/analytics" },
                 { icon: <KeyboardIcon />, title: "Chat", href: "/chat" },
                 { icon: <Tv2Icon />, title: "Team Meet", href: "/team-meet" },
@@ -98,7 +72,7 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
             ]}
         />
         <div className="flex flex-col w-full min-h-screen">
-            <div className="absolute inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:6rem_4rem]"><div className="absolute bottom-0 left-0 right-0 top-0 bg-[radial-gradient(circle_800px_at_100%_200px,#d5c5ff,transparent)]"></div></div>
+        <div className="absolute inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:6rem_4rem]"><div className="fix w-full bottom-0 right-0 top-0 bg-[radial-gradient(circle_800px_at_100%_200px,#d5c5ff,transparent)]"></div></div>
             <main className="flex-1 p-4 md:p-6 lg:p-8 xl:p-10">
                 <div className="grid gap-6 sm:grid-cols-2">
                     {children}
