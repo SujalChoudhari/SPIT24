5e1f6b148afc682d3d81e89951678bd2731afe36
Soham <realsoham774@gmail.com>
2024-02-10 13:44:19 +0530

    added kanban board

---
+++ src/app/tasks/page.tsx
@@ -0,0 +1,13 @@
+import KanbanBoard from '@/components/KanbanBoard'
+import React from 'react'
+
+export default function Tasks() {
+  return (
+    <div>
+      <h1 className='pt-10 ml-10 text-lg font-extrabold'>Tasks</h1>
+      <KanbanBoard/>  
+    </div>
+    
+  )
+}
+
