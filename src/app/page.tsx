49699fbfeb7921b0389a238fea4fad601535c8ab
Sujal Choudhari <sjlchoudhari@gmail.com>
2024-02-11 07:06:52 +0530

    Added Hero Component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import Patientloginin from "./(auth)/signup/page";
+import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
     <main>
-      <Patientloginin/>
+      <Hero />
+      {/* Landing */}
     </main>
   );
 }
---
+++ src/components/Hero.tsx
@@ -0,0 +1,5 @@
+export default function Hero() {
+    return <div className="w-[100lvw] h-[100lvh]" >
+        <iframe src="./landing/index.html" width="100%" height="100%" />
+    </div>
+}
